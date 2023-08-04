d9787ffeb5dae945c5f745b79340e32c7fac5112
Nahimana <mnahim024@gmail.com>
2023-08-04 03:25:51 +0200

    ♻️ F01: LOGIN > Create loginForm

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,36 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
+import LoginForm from "./LoginForm";
 
 const LoginPage = () => {
-  // state
-  const [prenom, setPrenom] = useState("");
-
-  //   comportements
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Bonjour ${prenom}`);
-    setPrenom("");
-  };
-  const handleChange = (e) => {
-    setPrenom(e.target.value);
-  };
-
-  return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <form action="#" onSubmit={handleSubmit}>
-        <input
-          value={prenom}
-          type="text"
-          placeholder="entrer votre prénom..."
-          required
-          onChange={handleChange}
-        />
-        <button type="submit">Accédez à votre espace</button>
-      </form>
-    </div>
-  );
+  return <LoginForm />;
 };
 
 export default LoginPage;
