32d72e7448d7155028c1c5fa80a04c4591da635f
Nahimana <mnahim024@gmail.com>
2023-08-05 20:52:47 +0200

    ✨ F02 : errorPage > add button

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
     <div>
       <h1>ErrorPage</h1>
+      <button onClick={handleClick}>Retourner à la page d'accueil</button>
     </div>
   );
 };
