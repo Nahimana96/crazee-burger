b1b5f526bf9b8bdcb0dfc1f7fea439cc248a90bb
Nahimana <mnahim024@gmail.com>
2023-08-05 20:50:12 +0200

    ✨ F02 : OrderPage > add new features

--- src/components/pages/OrderPage.jsx
+++ src/components/pages/OrderPage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const OrderPage = () => {
+  const { prenom } = useParams();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
     <div>
-      <h1>Bonjour Moussa</h1>
-      <button> Déconnexion</button>
+      <h1>Bonjour {prenom}</h1>
+      <button onClick={handleClick}> Déconnexion</button>
     </div>
   );
 };
