daa3b88fccaf83d7acf316bf2f0fd3e24671fc6b
Nahimana <mnahim024@gmail.com>
2023-08-05 21:51:53 +0200

    ♻️ F02 : store files in appropriate folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import LoginPage from "./components/pages/LoginPage";
+import LoginPage from "./components/pages/login/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import OrderPage from "./components/pages/OrderPage";
-import NotFound from "./components/pages/NotFound";
+import OrderPage from "./components/pages/order/OrderPage";
+import NotFound from "./components/pages/error/NotFound";
 function App() {
   return (
     <Router>
