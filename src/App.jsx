580e39150ab822c207dd3c07de60013dd2aa9e27
Nahimana <mnahim024@gmail.com>
2023-08-05 20:36:55 +0200

    ✨ F02 : Routes > create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import LoginPage from "./components/pages/LoginPage";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import OrderPage from "./components/pages/OrderPage";
+import NotFound from "./components/pages/NotFound";
 function App() {
   return (
-    <>
-      <LoginPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/order/:prenom" element={<OrderPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
