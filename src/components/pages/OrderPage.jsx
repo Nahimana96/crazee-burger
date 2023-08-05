274f6f9074e5f7f1d0dbc866de12afcd35dd2e94
Nahimana <mnahim024@gmail.com>
2023-08-05 19:59:31 +0200

    ✨ F02 :  OrderPage > Create the order page

---
+++ src/components/pages/OrderPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OrderPage = () => {
+  return (
+    <div>
+      <h1>Bonjour Moussa</h1>
+      <button> Déconnexion</button>
+    </div>
+  );
+};
+
+export default OrderPage;
