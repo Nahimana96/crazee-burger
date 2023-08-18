06c751d53a19c78679163650a5c226bc969e7bdc
Nahimana <mnahim024@gmail.com>
2023-08-18 23:11:41 +0200

    💄 F04 : OrderPage > Add style to the Navbar

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,13 +1,74 @@
 import React from "react";
 import Logo from "../../reusable-ui/Logo";
+import { styled } from "styled-components";
+import { theme } from "../../../theme";
+import { BsPersonCircle } from "react-icons/bs";
 const Navbar = ({ prenom, onClick }) => {
   return (
-    <div>
+    <NavbarStyled>
       <Logo />
-      <h1>Hey, {prenom}</h1>
-      <button onClick={onClick}> Déconnexion</button>
-    </div>
+      <div className="right-part">
+        <div className="logout-section">
+          <p className="hey-user">
+            Hey, <span>{prenom}</span>
+          </p>
+          <button className="logout" onClick={onClick}>
+            {" "}
+            Se déconnecter
+          </button>
+        </div>
+        <BsPersonCircle />
+      </div>
+    </NavbarStyled>
   );
 };
-
+const NavbarStyled = styled.div`
+  background-color: ${theme.colors.white};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
+  margin-top: 24px;
+  .right-part {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .logout-section {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-end;
+      .logout,
+      p {
+        margin: 0;
+        font-family: "Open Sans", sans-serif;
+        color: ${theme.colors.greyBlue};
+      }
+      > p {
+        font-size: ${theme.fonts.P0};
+        span {
+          font-weight: ${theme.weights.bold};
+          color: ${theme.colors.primary};
+        }
+      }
+      .logout {
+        border: 1px solid white;
+        background: none;
+        padding: 0;
+        font-size: ${theme.fonts.XXS};
+        transition: all 0.2s ease-in-out;
+        &:hover {
+          border-bottom: 1px solid black;
+          transition: all 0.2s ease-in-out;
+        }
+      }
+    }
+    svg {
+      color: ${theme.colors.greyBlue};
+      margin: 0 50px 0 15px;
+      scale: 1.8;
+    }
+  }
+`;
 export default Navbar;
