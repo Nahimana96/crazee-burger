2a49c22c5b3d514d55d2bb8de505b102a3c4f803
Nahimana <mnahim024@gmail.com>
2023-11-26 15:01:19 +0100

    ✨ F08 : MENU > add a delete button in products

--- src/components/pages/order/Menu/Product.jsx
+++ src/components/pages/order/Menu/Product.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import PrimaryButton from "../../../reusable-ui/PrimaryButton.jsx";
 import { formatPrice } from "../../../../utils/maths.jsx";
 import { styled } from "styled-components";
 import { theme } from "../../../../theme/index.jsx";
+import { TiDelete } from "react-icons/ti";
+import AdminContext from "../../../../context/AdminContext.jsx";
+
 const Product = ({ title, price, imageSource }) => {
+  const { isModeAdmin } = useContext(AdminContext);
   return (
     <ProductStyled>
+      {isModeAdmin && (
+        <div className="delete-button">
+          <TiDelete />
+        </div>
+      )}
       <div className="image-product">
         <img src={imageSource} alt={title} />
       </div>
@@ -20,6 +29,7 @@ const Product = ({ title, price, imageSource }) => {
   );
 };
 const ProductStyled = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -27,6 +37,21 @@ const ProductStyled = styled.div`
   padding: 50px 20px 30px 20px;
   box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
   border-radius: ${theme.borderRadius.extraRound};
+  .delete-button {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 15px;
+    right: 15px;
+    svg {
+      width: 100%;
+      height: 100%;
+      color: ${theme.colors.primary};
+    }
+  }
   .image-product {
     width: 200px;
     height: 145px;
