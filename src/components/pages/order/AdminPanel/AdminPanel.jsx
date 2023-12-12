e7d11e684f8222d467d4cc4e0aefaca9b69d2ab1
Nahimana <mnahim024@gmail.com>
2023-12-12 10:30:35 +0100

    ✨ F08 : AdminPanel > Create EditForm

--- src/components/pages/order/AdminPanel/AdminPanel.jsx
+++ src/components/pages/order/AdminPanel/AdminPanel.jsx
@@ -5,18 +5,19 @@ import AdminTabs from "./AdminTabs";
 import AdminContext from "../../../../context/AdminContext";
 import { getTabSelected, getTabsConfig } from "./getTabsConfig";
 import AddForm from "./AddForm";
+import EditForm from "./EditForm";
 const AdminPanel = () => {
   const { isPanelOpened, currentTabSelected } = useContext(AdminContext);
 
   const tabs = getTabsConfig;
-  const tabSelected = getTabSelected(tabs, currentTabSelected);
+  // const tabSelected = getTabSelected(tabs, currentTabSelected);
   return (
     <AdminPanelStyled>
       <div
         className={`panel-container ${isPanelOpened ? "appear" : "disappear"}`}
       >
         {currentTabSelected === "add" && <AddForm />}
-        {currentTabSelected === "edit" && <div>Modifier un produit</div>}
+        {currentTabSelected === "edit" && <EditForm />}
         <AdminTabs />
       </div>
     </AdminPanelStyled>
