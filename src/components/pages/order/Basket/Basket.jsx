fe82496eedb32ec459a092ea1e0b0774191ffa47
Nahimana <mnahim024@gmail.com>
2023-12-31 21:44:14 +0100

    ⚡️F11 : Basket > update the total price of items in cart

--- src/components/pages/order/Basket/Basket.jsx
+++ src/components/pages/order/Basket/Basket.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import Total from "./Total";
 import { formatPrice } from "../../../../utils/maths.jsx";
 import Footer from "./Footer.jsx";
 import BasketContent from "./BasketContent.jsx";
+import AdminContext from "../../../../context/AdminContext.jsx";
 const Basket = () => {
+  const { productsInBasket } = useContext(AdminContext);
+  const total = formatPrice(
+    productsInBasket.reduce((sum, item) => sum + item.price * item.quantity, 0)
+  );
   return (
     <BasketStyled>
-      <Total amountToPay={formatPrice(0)} />
+      <Total amountToPay={total} />
       <BasketContent />
       <Footer />
     </BasketStyled>
